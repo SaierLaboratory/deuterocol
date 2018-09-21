cfe03ee6ea8a971756f8359099f0d39537d4d15a
K H <khendarg@ucsd.edu>
2018-09-21 15:55:21 -0700

    a partially working configuration

--- extendomatic.py
+++ extendomatic.py
@@ -8,7 +8,7 @@ import scipy.stats
 import sys
 
 PRIOR = 0.5
-BEAUTYFACTOR = 1
+BEAUTYFACTOR = 8
 
 #import matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -217,12 +217,12 @@ class Dataset(object):
 			elif l.startswith('#'): continue
 
 			sl = l.split('\t')
-			names.append(sl[0])
 			obj = json.loads(sl[1])
 			rmsd, length, mincov, maxcov, minpresent = unpack_obj(obj)
 			if minpresent < self.min_present: continue
 			if rmsd == -1: continue
 
+			names.append(sl[0])
 			rmsds.append(rmsd)
 			lengths.append(length)
 			mincovs.append(mincov)
@@ -309,9 +309,7 @@ def plot_3d_posteriors(pkde, nkde, fig, ax, rmsdlim=(0., 6.), mincovlim=(0., 1.)
 	X, Y = np.mgrid[rmsdlim[0]:rmsdlim[1]:resolution*1j, mincovlim[0]:mincovlim[1]:resolution*1j]
 	positions = np.vstack([X.ravel(), Y.ravel()])
 
-	if PRIOR == 'scaled': posteriors = pkde.evaluate(positions) * len(pkde.mincovs) / (pkde.evaluate(positions) * len(pkde.mincovs) + nkde.evaluate(positions) * len(nkde.mincovs))
-	elif PRIOR == 'noninformative': posteriors = pkde.evaluate(positions) / (pkde.evaluate(positions) + nkde.evaluate(positions))
-	else: posteriors = pkde.evaluate(positions) * PRIOR / (pkde.evaluate(positions) * PRIOR + nkde.evaluate(positions) * (1-PRIOR))
+	posteriors = pkde.evaluate(positions) * PRIOR / (pkde.evaluate(positions) * PRIOR + nkde.evaluate(positions) * (1-PRIOR))
 
 	Z = np.reshape(posteriors.T, X.shape)
 
@@ -326,13 +324,11 @@ def plot_3d_posteriors(pkde, nkde, fig, ax, rmsdlim=(0., 6.), mincovlim=(0., 1.)
 	ax.set_title('Posteriors (n={}+{})'.format(len(pkde.mincovs), len(nkde.mincovs)))
 	fig.colorbar(surf, ax=ax)
 
-def plot_posteriors(pkde, nkde, fig, ax, rmsdlim=(0., 6.), mincovlim=(0., 1.), resolution=100):
+def plot_posteriors(pkde, nkde, fig, ax, rmsdlim=(0., 6.), mincovlim=(0., 1.), resolution=100, colorbar=True):
 	X, Y = np.mgrid[rmsdlim[0]:rmsdlim[1]:resolution*1j, mincovlim[0]:mincovlim[1]:resolution*1j]
 	positions = np.vstack([X.ravel(), Y.ravel()])
 
-	if PRIOR == 'scaled': posteriors = pkde.evaluate(positions) * len(pkde.mincovs) / (pkde.evaluate(positions) * len(pkde.mincovs) + nkde.evaluate(positions) * len(nkde.mincovs))
-	elif PRIOR == 'noninformative': posteriors = pkde.evaluate(positions) / (pkde.evaluate(positions) + nkde.evaluate(positions))
-	else: posteriors = pkde.evaluate(positions) * PRIOR / (pkde.evaluate(positions) * PRIOR + nkde.evaluate(positions) * (1-PRIOR))
+	posteriors = pkde.evaluate(positions) * PRIOR / (pkde.evaluate(positions) * PRIOR + nkde.evaluate(positions) * (1-PRIOR))
 
 	Z = np.reshape(posteriors.T, X.shape)
 
@@ -343,10 +339,69 @@ def plot_posteriors(pkde, nkde, fig, ax, rmsdlim=(0., 6.), mincovlim=(0., 1.), r
 	ax.set_xlabel('RMSD')
 	ax.set_ylabel('Coverage')
 
-	fig.colorbar(im, ax=ax)
+	if colorbar: fig.colorbar(im, ax=ax)
 
 	ax.set_title('Posteriors (n={}+{})'.format(len(pkde.mincovs), len(nkde.mincovs)))
 	
+def plot_independent_posteriors(positive, negative, figure, ax, rmsdlim, mincovlim, resolution=100, rbw=(1., 1.), cbw=(1., 1.)):
+	rmsds, mincovs = np.mgrid[rmsdlim[0]:rmsdlim[1]:resolution*1j, \
+		mincovlim[0]:mincovlim[1]:resolution*1j]
+	positions = np.vstack([rmsds.ravel(), mincovs.ravel()])
+
+	pos_rkde = scipy.stats.gaussian_kde(positive.rmsds)
+	pos_ckde = scipy.stats.gaussian_kde(positive.mincovs)
+	neg_rkde = scipy.stats.gaussian_kde(negative.rmsds)
+	neg_ckde = scipy.stats.gaussian_kde(negative.mincovs)
+	pos_rkde.set_bandwidth(bw_method=pos_rkde.factor*rbw[0])
+	pos_ckde.set_bandwidth(bw_method=pos_ckde.factor*cbw[0])
+	neg_rkde.set_bandwidth(bw_method=neg_rkde.factor*rbw[1])
+	neg_ckde.set_bandwidth(bw_method=neg_ckde.factor*cbw[1])
+
+	rvalues = pos_rkde.evaluate(positions[0]) * PRIOR / (pos_rkde.evaluate(positions[0]) * PRIOR + neg_rkde.evaluate(positions[0]) * (1 - PRIOR))
+	cvalues = pos_ckde.evaluate(positions[1]) * PRIOR / (pos_ckde.evaluate(positions[1]) * PRIOR + neg_ckde.evaluate(positions[1]) * (1 - PRIOR))
+	values = rvalues * cvalues
+	Z = np.reshape(values.T, rmsds.shape)
+
+	aspect = (rmsdlim[1] - rmsdlim[0]) / (mincovlim[1] - mincovlim[0]) / 1.414
+	im = ax.imshow(np.rot90(Z), cmap='magma', extent=rmsdlim+mincovlim, aspect=aspect)
+
+	ax.set_xlim(rmsdlim)
+	ax.set_ylim(mincovlim)
+	ax.set_xlabel('RMSD')
+	ax.set_ylabel('Coverage')
+	ax.set_title('Posteriors (n={}+{})'.format(len(positive.mincovs), len(negative.mincovs)))
+	figure.colorbar(im, ax=ax)
+
+
+def plot_3d_independent_posteriors(positive, negative, fig, ax, rmsdlim, mincovlim, resolution=200, rbw=(1., 1.), cbw=(1., 1.)):
+	rmsds, mincovs = np.mgrid[rmsdlim[0]:rmsdlim[1]:resolution*1j, \
+		mincovlim[0]:mincovlim[1]:resolution*1j]
+	positions = np.vstack([rmsds.ravel(), mincovs.ravel()])
+
+	pos_rkde = scipy.stats.gaussian_kde(positive.rmsds)
+	pos_ckde = scipy.stats.gaussian_kde(positive.mincovs)
+	neg_rkde = scipy.stats.gaussian_kde(negative.rmsds)
+	neg_ckde = scipy.stats.gaussian_kde(negative.mincovs)
+	pos_rkde.set_bandwidth(bw_method=pos_rkde.factor*rbw[0])
+	pos_ckde.set_bandwidth(bw_method=pos_ckde.factor*cbw[0])
+	neg_rkde.set_bandwidth(bw_method=neg_rkde.factor*rbw[1])
+	neg_ckde.set_bandwidth(bw_method=neg_ckde.factor*cbw[1])
+
+	rvalues = pos_rkde.evaluate(positions[0]) * PRIOR / (pos_rkde.evaluate(positions[0]) * PRIOR + neg_rkde.evaluate(positions[0]) * (1 - PRIOR))
+	cvalues = pos_ckde.evaluate(positions[1]) * PRIOR / (pos_ckde.evaluate(positions[1]) * PRIOR + neg_ckde.evaluate(positions[1]) * (1 - PRIOR))
+	values = rvalues * cvalues
+	Z = np.reshape(values.T, rmsds.shape)
+
+	surf = ax.plot_surface(rmsds, mincovs, Z, cmap='magma', linewidth=0, antialiased=False)
+	ax.set_xlim(rmsdlim)
+	ax.set_ylim(mincovlim)
+	ax.set_zlim(0.0, 1.0)
+	ax.set_xlabel('RMSD')
+	ax.set_ylabel('Coverage')
+	ax.set_zlabel('p')
+	ax.set_title('Posteriors (n={}+{})'.format(len(positive.mincovs), len(negative.mincovs)))
+	fig.colorbar(surf, ax=ax)
+
 def plot_univariate_densities(dataset, fig, ax1, ax2, lim1, lim2, resolution=200, rbw=1., cbw=1.):
 	X1 = np.arange(lim1[0], lim1[1], (lim1[1] - lim1[0])/resolution)
 	X2 = np.arange(lim2[0], lim2[1], (lim2[1] - lim2[0])/resolution)
@@ -377,9 +432,7 @@ def plot_univariate_posteriors(pvalues, nvalues, fig, ax, lim, resolution=200, x
 
 	pkernel.set_bandwidth(bw_method=pkernel.factor*bw[0])
 	nkernel.set_bandwidth(bw_method=nkernel.factor*bw[1])
-	if PRIOR == 'scaled': posteriors = pkernel.evaluate(X)  * len(pvalues) / (pkernel.evaluate(X) * len(pvalues) + nkernel.evaluate(X) * len(nvalues))
-	elif PRIOR == 'noninformative': posteriors = pkernel.evaluate(X) / (pkernel.evaluate(X) + nkernel.evaluate(X))
-	else: posteriors = pkernel.evaluate(X)  * PRIOR / (pkernel.evaluate(X) * PRIOR + nkernel.evaluate(X) * (1-PRIOR))
+	posteriors = pkernel.evaluate(X)  * PRIOR / (pkernel.evaluate(X) * PRIOR + nkernel.evaluate(X) * (1-PRIOR))
 	ax.plot(X, posteriors)
 	ax.set_xlim(lim)
 	ax.set_ylim([0, 1])
@@ -388,10 +441,49 @@ def plot_univariate_posteriors(pvalues, nvalues, fig, ax, lim, resolution=200, x
 	#ax.plot(kde.rmsds, kde.mincovs, c=(1., 1., 1., 0.1), marker='.', linewidth=0)
 
 
-def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0., 1.), resolution=100, density_outfile='kde_plot.png', scatter_outfile='kde_scatter.png', text='', posterior_outfile='kde_posterior.png', univar_post_outfile='univar_posterior.png', post_surf_outfile='posterior_surface.png', unklabel='Unknown', dens_surf_outfile='dens_surf_plot.png', univar_dens_outfile='univar_dens_plot.png', rbw=(1.75,1.75,1.75), cbw=(1.25,2.,1.5)):
-
+#def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0., 1.), resolution=100, density_outfile='kde_plot.png', scatter_outfile='kde_scatter.png', text='', posterior_outfile='kde_posterior.png', univar_post_outfile='univar_posterior.png', post_surf_outfile='posterior_surface.png', unklabel='Unknown', dens_surf_outfile='dens_surf_plot.png', univar_dens_outfile='univar_dens_plot.png', rbw=(1.75,1.75,1.75), cbw=(1.25,2.,1.5), indep_post_outfile='indep_post_plot.png', dpi=1200):
+def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0., 1.), resolution=400, density_outfile='kde_plot.png', scatter_outfile='kde_scatter.png', text='', posterior_outfile='kde_posterior.png', univar_post_outfile='univar_posterior.png', post_surf_outfile='posterior_surface.png', unklabel='Unknown', dens_surf_outfile='dens_surf_plot.png', univar_dens_outfile='univar_dens_plot.png', rbw=(1.75,1.75,1.75), cbw=(1.25,2.,1.5), indep_post_outfile='indep_post_plot.png', dpi=600, univar_surf_outfile='univar_surf_plot.png', univar_postsurf_outfile='univar_postsurf.png'):
 
 	print(rbw, cbw)
+	##posteriors in 2D
+	#figure = Figure()
+	#canvas = FigureCanvas(figure)
+	#ax = figure.add_subplot(1, 1, 1)
+	#plot_posteriors(positive, negative, fig=figure, ax=ax, rmsdlim=rmsdlim, mincovlim=mincovlim, resolution=resolution)
+	#figure.savefig(posterior_outfile, dpi=dpi)
+	#exit()
+
+	#a bunch of posteriors in 2D
+	figure = Figure()
+	canvas = FigureCanvas(figure)
+	ax = []
+
+	poscbws = (1., 2., 3., 4., 5., 6., 7., 8.)
+	poscbws = np.arange(1., 5., 0.5)
+	negcbws = (1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14.)
+	negcbws = np.arange(4., 7., 0.5)
+
+	exit()
+	for i in range(len(poscbws)*len(negcbws)):
+		ax.append(figure.add_subplot(len(poscbws), len(negcbws), i+1))
+		ax[-1].tick_params(labelsize=3.)
+	#figure.tight_layout()
+	i = 0
+	for poscbw in poscbws:
+		positive.kernel.set_bandwidth(bw_method=positive.kernel.factor * poscbw)
+		for negcbw in negcbws:
+			negative.kernel.set_bandwidth(bw_method=negative.kernel.factor * negcbw)
+			plot_posteriors(positive, negative, fig=figure, ax=ax[i], rmsdlim=rmsdlim, mincovlim=mincovlim, resolution=resolution/2, colorbar=False)
+			ax[i].set_title('{}, {}'.format(poscbw, negcbw), fontsize=4.)
+			ax[i].set_xlabel('', fontsize=3.)
+			ax[i].set_ylabel('', fontsize=3.)
+			ax[i].plot([2.8], [0.68], marker='+', color=(0.,1.,0.,0.3))
+			i += 1
+			negative.kernel.set_bandwidth(bw_method=negative.kernel.factor / negcbw)
+		positive.kernel.set_bandwidth(bw_method=positive.kernel.factor / poscbw)
+	figure.savefig(posterior_outfile, dpi=dpi)
+	exit()
+
 	#univariate densities
 	figure = Figure()
 	canvas = FigureCanvas(figure)
@@ -417,7 +509,7 @@ def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0.,
 	ax[2].set_title('Negative (n={})'.format(len(negative.mincovs)))
 	plot_univariate_densities(unknown, figure, ax[4], ax[5], lim1=rmsdlim, lim2=mincovlim, resolution=2*resolution, rbw=rbw[2], cbw=cbw[2])
 	ax[4].set_title('{} (n={})'.format(unklabel, len(unknown.mincovs)))
-	figure.savefig(univar_dens_outfile, dpi=600)
+	figure.savefig(univar_dens_outfile, dpi=dpi)
 
 	#univariate posteriors
 	figure = Figure()
@@ -430,10 +522,14 @@ def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0.,
 
 	plot_univariate_posteriors(positive.rmsds, negative.rmsds, figure, ax[0], lim=rmsdlim, resolution=2*resolution, xlabel='RMSD', ylabel='p', bw=rbw)
 	plot_univariate_posteriors(positive.mincovs, negative.mincovs, figure, ax[1], lim=mincovlim, resolution=2*resolution, xlabel='Coverage', ylabel='p', bw=cbw)
-	figure.savefig(univar_post_outfile, dpi=600)
+	figure.savefig(univar_post_outfile, dpi=dpi)
 
-	exit()
-
+	##2D univariate posterior plot
+	#figure = Figure()
+	#canvas = FigureCanvas(figure)
+	#ax = figure.add_subplot(1, 1, 1)
+	#plot_independent_posteriors(positive, negative, figure, ax, rmsdlim=rmsdlim, mincovlim=mincovlim, resolution=resolution, rbw=rbw, cbw=cbw)
+	#figure.savefig(univar_postsurf_outfile, dpi=dpi)
 
 	#density plots
 	figure = Figure()
@@ -455,25 +551,18 @@ def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0.,
 	ax[3].axis([0, 1, 0, 1])
 	ax[3].text(0.0, 0.0, text, fontsize=5., fontname='monospace')
 
-	figure.savefig(density_outfile, dpi=600)
+	figure.savefig(density_outfile, dpi=dpi)
 	plot_rmsd_cov(positive, fig=figure, ax=ax[0])
 	plot_rmsd_cov(negative, fig=figure, ax=ax[1])
 	plot_rmsd_cov(unknown, fig=figure, ax=ax[2])
-	figure.savefig(scatter_outfile, dpi=600)
-
-	#posteriors in 2D
-	figure = Figure()
-	canvas = FigureCanvas(figure)
-	ax = figure.add_subplot(1, 1, 1)
-	plot_posteriors(positive, negative, fig=figure, ax=ax, rmsdlim=rmsdlim, mincovlim=mincovlim, resolution=resolution)
-	figure.savefig(posterior_outfile, dpi=600)
+	figure.savefig(scatter_outfile, dpi=dpi)
 
 	#posteriors in 3D
 	figure = Figure()
 	canvas = FigureCanvas(figure)
 	ax = figure.add_subplot(1, 1, 1, projection='3d')
 	plot_3d_posteriors(positive, negative, fig=figure, ax=ax, rmsdlim=rmsdlim, mincovlim=mincovlim, resolution=resolution*BEAUTYFACTOR)
-	figure.savefig(post_surf_outfile, dpi=600)
+	figure.savefig(post_surf_outfile, dpi=dpi)
 
 	#densities in 3D
 	figure = Figure()
@@ -496,14 +585,22 @@ def plot_densities(positive, negative, unknown, rmsdlim=(0., 6.), mincovlim=(0.,
 	ax[2].set_zlim([ax[2].get_zlim()[0], maxz])
 	ax[3].axis([0, 1, 0, 1])
 	ax[3].text(0.0, 0.0, text, fontsize=5., fontname='monospace')
-	figure.savefig(dens_surf_outfile, dpi=600)
+	figure.savefig(dens_surf_outfile, dpi=dpi)
+
+	#3D univariate posterior plots
+	figure = Figure()
+	canvas = FigureCanvas(figure)
+	ax = figure.add_subplot(1, 1, 1, projection='3d')
+	plot_3d_independent_posteriors(positive, negative, figure, ax, rmsdlim=rmsdlim, mincovlim=mincovlim, resolution=BEAUTYFACTOR*resolution, rbw=rbw, cbw=cbw)
+	figure.savefig(univar_surf_outfile, dpi=dpi)
+	exit()
 
 
 def plot_rmsd_cov(kde, fig, ax):
 	ax.plot(kde.rmsds, kde.mincovs, c=(1., 1., 1., 0.1), marker='.', linewidth=0)
 
 
-def main(positivefn, negativefn, unknownfn, count=1000, density_outfile='density_plot.png', scatter_outfile='scatter_plot.png', stretch=1, posterior_outfile='posterior_plot.png', univar_post_outfile='univar_posterior_plot.png', post_surf_outfile='post_surf_plot.png', min_present=50, unklabel='Unknown', dens_surf_outfile='dens_surf_plot.png', univar_dens_outfile='univar_dens_plot.png'):
+def main(positivefn, negativefn, unknownfn, count=1000, density_outfile='density_plot.png', scatter_outfile='scatter_plot.png', stretch=1, posterior_outfile='posterior_plot.png', univar_post_outfile='univar_posterior_plot.png', post_surf_outfile='post_surf_plot.png', min_present=50, unklabel='Unknown', dens_surf_outfile='dens_surf_plot.png', univar_dens_outfile='univar_dens_plot.png', indep_post_outfile='indep_post_plot.png', univar_surf_outfile='univar_surf_plot.png', univar_postsurf_outfile='univar_postsurf_plot.png'):
 	with open(positivefn) as f: positive = Dataset(f, count=count, mode='any', marg=stretch, min_present=min_present)
 	positive.gen_rmsd_mincov_kde()
 	#with open(negativefn) as f: negative = Dataset(f, count=count, mode='any', marg=stretch, min_present=min_present)
@@ -523,35 +620,38 @@ def main(positivefn, negativefn, unknownfn, count=1000, density_outfile='density
 	#rbw = (1.75, 1.75, 1.75)
 	#cbw = (1., 2., 1.5)
 
+	#previous settings
+	#cbw = (1.0, 2.25, 1.5)
+
 	#this eliminates the low-cov peak, altering as little as possible
+	
 	rbw = (1.25, 1.25, 1.25)
-	cbw = (1., 1.8, 1.5)
+	cbw = (2.0, 3.0, 1.5)
 
 	#
 	#rbw = (2.25, 2.25, 2.25)
 	#cbw = (1., 2., 1.5)
 
-	positive.kernel.set_bandwidth(bw_method=positive.kernel.factor*cbw[0])
-	positive.kernel.set_bandwidth(bw_method=positive.kernel.factor*cbw[1])
-	positive.kernel.set_bandwidth(bw_method=positive.kernel.factor*cbw[2])
-	#print(positive.kernel.factor)
-	#print(negative.kernel.factor)
-	#print(unknown.kernel.factor)
-	#exit()
+	cbw = (1., 1., 1.5)
 
-	if PRIOR == 'scaled': unkposteriors = positive.evaluate(unkpoints) * len(positive.rmsds) / (positive.evaluate(unkpoints) * len(positive.rmsds) + negative.evaluate(unkpoints) * len(negative.rmsds))
-	elif PRIOR == 'noninformative': unkposteriors = positive.evaluate(unkpoints) / (positive.evaluate(unkpoints) + negative.evaluate(unkpoints) )
-	else: unkposteriors = positive.evaluate(unkpoints) * PRIOR / (positive.evaluate(unkpoints) * PRIOR + negative.evaluate(unkpoints) * (1 - PRIOR))
+	positive.kernel.set_bandwidth(bw_method=positive.kernel.factor*cbw[0])
+	negative.kernel.set_bandwidth(bw_method=positive.kernel.factor*cbw[1])
+	unknown.kernel.set_bandwidth(bw_method=positive.kernel.factor*cbw[2])
+
+	unkposteriors = positive.evaluate(unkpoints) * PRIOR / (positive.evaluate(unkpoints) * PRIOR + negative.evaluate(unkpoints) * (1 - PRIOR))
 	#text += '#Alignment\tPosterior\tRMSD\tCoverage\n'
 	text += '#Alignment'.ljust(32)
 	text += 'Posterior'.ljust(12)
 	text += 'RMSD'.ljust(6)
 	text += 'Coverage\n'
+	for name, post, rmsd in zip(unknown.names, unkposteriors, unknown.rmsds): 
+		if post < 0.7: continue
+		print('{}\t{}\t{}'.format(name, post, rmsd))
 	for post, name, rmsd, mincov in sorted(zip(unkposteriors, unknown.names, unknown.rmsds, unknown.mincovs))[::-1][:10]:
 		#text += '{}\t{:0.02e}\t{:0.1f}\t{:0.0%}\n'.format(name, post, rmsd, mincov)
 		text += '{}'.format(name).ljust(32)
 		text += '{:0.02e}'.format(post).ljust(12)
-		text += '{:0.1f}'.format(rmsd).ljust(6)
+		text += '{:0.4f}'.format(rmsd).ljust(9)
 		text += '{:0.0%}\n'.format(mincov)
 	print(text)
 	plot_densities(positive, negative, unknown, rmsdlim=(min_rmsd, max_rmsd), mincovlim=(0.0, max_mincov), resolution=100, 
@@ -564,6 +664,8 @@ def main(positivefn, negativefn, unknownfn, count=1000, density_outfile='density
 		univar_post_outfile=univar_post_outfile, 
 		post_surf_outfile=post_surf_outfile,
 		dens_surf_outfile=dens_surf_outfile, 
+		indep_post_outfile=indep_post_outfile,
+		univar_surf_outfile=univar_surf_outfile,
 		rbw=rbw, 
 		cbw=cbw)
 
@@ -580,15 +682,18 @@ if __name__ == '__main__':
 	parser.add_argument('unknown')
 	parser.add_argument('--min-present', type=int, default=50)
 	parser.add_argument('-n', type=int, default=1000)
-	parser.add_argument('-o', nargs=7, default=['density_plot.png', 'scatter_plot.png', 'posterior_plot.png', 'univar_post_plot.png', 'post_surf_plot.png', 'dens_surf_plot.png', 'univar_dens_plot.png'])
+	parser.add_argument('-o', nargs=9, default=['density_plot.png', 'scatter_plot.png', 'posterior_plot.png', 'univar_post_plot.png', 'post_surf_plot.png', 'dens_surf_plot.png', 'univar_dens_plot.png', 'indep_post_plot.png', 'univar_surf_plot.png', 'univar_postsurf_plot.png'])
 	parser.add_argument('-s', default=1, type=int)
 	parser.add_argument('--unklabel', default='Unknown')
+	parser.add_argument('--beauty-factor', type=int, default=BEAUTYFACTOR, help='extra resolution for 3d plots (warning: drastically increases runtime)')
 
 	parser.add_argument('--prior', type=float)
 
 	args = parser.parse_args()
 
-	if args.prior is None: PRIOR = 'noninformative'
+	if args.prior is None: PRIOR = 0.5
 	else: PRIOR = args.prior
 
-	main(args.positive, args.negative, args.unknown, count=args.n, density_outfile=args.o[0], stretch=args.s, scatter_outfile=args.o[1], posterior_outfile=args.o[2], univar_post_outfile=args.o[3], post_surf_outfile=args.o[4], dens_surf_outfile=args.o[5], min_present=args.min_present, unklabel=args.unklabel, univar_dens_outfile=args.o[6])
+	BEAUTYFACTOR = 8 if args.beauty_factor is None else args.beauty_factor
+
+	main(args.positive, args.negative, args.unknown, count=args.n, density_outfile=args.o[0], stretch=args.s, scatter_outfile=args.o[1], posterior_outfile=args.o[2], univar_post_outfile=args.o[3], post_surf_outfile=args.o[4], dens_surf_outfile=args.o[5], min_present=args.min_present, unklabel=args.unklabel, univar_dens_outfile=args.o[6], indep_post_outfile=args.o[7], univar_surf_outfile=args.o[8], univar_postsurf_outfile=args.o[9])
