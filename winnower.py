661767c17e8f50e0bf40c639ca549bda1f4e1c46
khendarg <khendarg@ucsd.edu>
2018-09-26 18:29:50 -0700

    added TMS filtering

--- winnower.py
+++ winnower.py
@@ -6,7 +6,7 @@ import argparse, os, sys, json
 import extendomatic
 
 class Alignment(object):
-	def __init__(self, name, jstr):
+	def __init__(self, name, jstr, tmdatadir=None):
 		self.name = name
 		self.query, self.qchain, self.qhel, vs, self.subject, self.schain, self.shel = name.split('_')
 		self.qhels = get_hel_list(self.qhel)
@@ -18,6 +18,10 @@ class Alignment(object):
 		self.length = data[1]
 		self.mincov = data[2]
 		self.maxcov = data[3]
+
+		self.qpresent = obj['qpresent']
+		self.spresent = obj['spresent']
+
 	def __lt__(self, other):
 		if self.mincov > other.mincov: return True
 		elif self.mincov == other.mincov:
@@ -35,10 +39,37 @@ def intersects(l1, l2):
 		if x1 in l2: return True
 	return False
 
+def load_opm_tmss(tmdatadir='tmdata'):
+	tmdict = {}
+	with open('{}/opm/ASSIGNMENTS.TSV'.format(tmdatadir)) as f:
+		for l in f: 
+			pdbid, tmstr = l.strip().split('\t')
+			pdbid = pdbid[:4].upper() + pdbid[4:]
+			tmdict[pdbid] = []
+			tmdict[pdbid[:4]] = []
+			for span in tmstr.split(','):
+				tmdict[pdbid].append([int(x) for x in span.split('-')])
+				tmdict[pdbid[:4]].append([int(x) for x in span.split('-')])
+	return tmdict
 
-def main(infile, outfile='/dev/stdout', stretch=0, append=False):
+def overlap(s1, s2):
+	r1 = set(range(*s1))
+	r2 = set(range(*s2))
+	return len(r1.intersection(r2))
+def count_covered_tmss(spans, interval, threshold=10):
+	tmss = 0
+	for span in spans:
+		n = overlap(span, interval)
+		if n > threshold: tmss += 1
+	return tmss
+
+def main(infile, outfile='/dev/stdout', stretch=0, append=False, tmdatadir='tmdata'):
 	print(infile, outfile)
+	if not tmdatadir: tmdatadir = None
 	currstrucs = (None, None)
+
+	tmdict = load_opm_tmss(tmdatadir)
+
 	donehels = ([], [])
 	#best = {}
 	best = []
@@ -53,7 +84,7 @@ def main(infile, outfile='/dev/stdout', stretch=0, append=False):
 			except ValueError:
 				print(l)
 				continue
-			try: current = Alignment(name, jstr)
+			try: current = Alignment(name, jstr, tmdatadir=tmdatadir)
 			except ValueError: continue
 			if current.rmsd == -1: continue
 			query, qchain, qhel, vs, subject, schain, shel = name.split('_')
@@ -61,6 +92,23 @@ def main(infile, outfile='/dev/stdout', stretch=0, append=False):
 			shels = get_hel_list(qhel)
 			qpdbid = '{}_{}'.format(query, qchain)
 			spdbid = '{}_{}'.format(subject, schain)
+
+			#eliminate alignments that couldn't ever exceed 2 TMSs
+			skip = False
+			try: qtmscount = count_covered_tmss(tmdict[qpdbid], current.qpresent[0])
+			except KeyError: 
+				try: qtmscount = count_covered_tmss(tmdict[qpdbid[:4]], current.qpresent[0])
+				except KeyError: qtmscount = 6.5 #arbitrary value to allow PDBTM-only structures for now
+			finally:
+				if qtmscount <= 2: skip = True
+			try: stmscount = count_covered_tmss(tmdict[spdbid], current.spresent[0])
+			except KeyError: 
+				try: stmscount = count_covered_tmss(tmdict[spdbid[:4]], current.spresent[0])
+				except KeyError: stmscount = 6.5 #arbitrary value to allow PDBTM-only structures for now
+			finally:
+				if stmscount <= 2: skip = True
+			if skip: continue
+
 			
 		#	if currstrucs == (qpdbid, spdbid):
 		#		if best[currstrucs][0].mincov < current.mincov:
@@ -113,11 +161,15 @@ if __name__ == '__main__':
 	parser.add_argument('infile', help='a single superposition TSV or a deuterocol2 root directory')
 	parser.add_argument('-s', default=0, type=int, help='how many alignments to keep for each pair of structures')
 	parser.add_argument('outfile', nargs='?', default='/dev/stdout')
+	parser.add_argument('--tmdatadir', nargs='?', default='tmdata')
 
 	args = parser.parse_args()
 	if args.infile == args.outfile: 
 		parser.print_usage()
 		exit(1)
+
+	if not os.path.isdir(args.tmdatadir): 
+		raise IOError('Could not find tmdata directory')
 
 	if os.path.isdir(args.infile):
 		open(args.outfile, 'w')
@@ -125,5 +177,5 @@ if __name__ == '__main__':
 			for spfn in os.listdir('{}/{}/superpositions'.format(args.infile, famvfam)):
 				if spfn.startswith('.'): continue
 				elif not spfn.lower().endswith('tsv'): continue
-				main('{}/{}/superpositions/{}'.format(args.infile, famvfam, spfn), args.outfile, stretch=args.s, append=True)
-	else: main(args.infile, args.outfile, stretch=args.s, append=False)
+				main('{}/{}/superpositions/{}'.format(args.infile, famvfam, spfn), args.outfile, stretch=args.s, append=True, tmdatadir=args.tmdatadir)
+	else: main(args.infile, args.outfile, stretch=args.s, append=False, tmdatadir=args.tmdatadir)
