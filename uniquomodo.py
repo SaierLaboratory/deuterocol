59243c5784a1236e86876c2bb6df1ea5f32907fa
K H <khendarg@ucsd.edu>
2018-11-19 19:51:59 -0800

    updated uniquomodo

--- uniquomodo.py
+++ uniquomodo.py
@@ -1,55 +1,82 @@
 #!/usr/bin/env python
 
 from __future__ import print_function, division
-import json, os
-import tempfile, urllib
-import subprocess
-import pymol
-import argparse
 import sys
+import json
+import argparse
+import shutil
+import tempfile
+
+import pymol
+
+def test_transform():
+
+	matrix = [
+     0.9164623314, -0.2389936591,  0.3209031413, 495.2727054608,  
+     0.1849898163, -0.4580664883, -0.8694560714, 132.6568206768,  
+     0.3547894629,  0.8561875514, -0.3755892888, 186.3692863698,  
+     0.000000000000,  0.0000000000,  0.0000000000,  1.0000000000, 
+	]
+
+	#matrix = [
+	#	10., 0., 0., 0.,
+	#	0., 1., 0., 0.,
+	#	0., 0., 1., 0.,
+	#	0., 0., 0., 1.,
+	#]
+
+	pymol.finish_launching()
+	pymol.cmd.load('2v8n.pdb')
+	pymol.cmd.load('4pyp.pdb')
+	help(pymol.cmd.transform_selection)
+	pymol.cmd.transform_selection('2v8n', matrix)
+	pymol.cmd.center('2v8n')
+	pass
+
+
 def extract_selectors(name, obj):
 	query, qchain, qtms, vs, subject, schain, stms = name.split('_')
+	seldict = {}
 
-	qmasked = '{} and c. {} and i. {}-{}'.format(
-		obj['query'],
+	seldict['qchain'] = '{} and c. {}'.format(query, qchain)
+	seldict['schain'] = '{} and c. {}'.format(subject, schain)
+	seldict['qmasked'] = '{} and c. {} and i. {}-{}'.format(
+		obj['query'], 
 		qchain,
 		obj['qpresent'][0][0],
-		obj['qpresent'][0][1],
+		obj['qpresent'][0][1]
 	)
-	smasked = '{} and c. {} and i. {}-{}'.format(
-		obj['subject'],
+	seldict['smasked'] = '{} and c. {} and i. {}-{}'.format(
+		obj['subject'], 
 		schain,
 		obj['spresent'][0][0],
-		obj['spresent'][0][1],
+		obj['spresent'][0][1]
 	)
-
 	alnq = []
 	for span in obj['qaligned']:
 		if span[0] is None: alnq += [None] * span[1]
-		else: alnq += list(range(span[0], span[1]+1))
+		else: alnq += list(range(span[0], span[0]+span[1]+1))
 	alns = []
 	for span in obj['saligned']:
 		if span[0] is None: alns += [None] * span[1]
-		else: alns += list(range(span[0], span[1]+1))
+		else: alns += list(range(span[0], span[0]+span[1]+1))
 
-	qpresent = '{} and c. {} and i. '.format(obj['query'], qchain)
-	spresent = '{} and c. {} and i. '.format(obj['subject'], schain)
-	qaligned = '{} and c. {} and i. '.format(obj['query'], qchain)
-	saligned = '{} and c. {} and i. '.format(obj['subject'], schain)
+	seldict['qpresent'] = '{} and c. {} and i. '.format(obj['query'], qchain)
+	seldict['spresent'] = '{} and c. {} and i. '.format(obj['subject'], schain)
+	seldict['qaligned'] = '{} and c. {} and i. '.format(obj['query'], qchain)
+	seldict['saligned'] = '{} and c. {} and i. '.format(obj['subject'], schain)
 
 	qp = obj['qpresent'][0][1] - obj['qpresent'][0][0]
 	sp = obj['spresent'][0][1] - obj['spresent'][0][0]
 	qa = 0
 	sa = 0
-	print(obj)
-	print(obj['distances'])
-	print(alnq, alns)
+
 	for q, d, s in zip(alnq, obj['distances'], alns):
-		if q is not None: qpresent += '{}+'.format(q)
-		if s is not None: spresent += '{}+'.format(s)
+		if q is not None: seldict['qpresent'] += '{}+'.format(q)
+		if s is not None: seldict['spresent'] += '{}+'.format(s)
 		if (q is not None) and (s is not None) and (d is not None):
-			qaligned += '{}+'.format(q)
-			saligned += '{}+'.format(s)
+			seldict['qaligned'] += '{}+'.format(q)
+			seldict['saligned'] += '{}+'.format(s)
 			qa += 1
 			sa += 1
 
@@ -60,92 +87,72 @@ def extract_selectors(name, obj):
 		print('\tMinimum coverage: {:0.2%} of query'.format(qa/qp))
 	else:
 		print('\tMinimum coverage: {:0.2%} of subject'.format(sa/sp))
-	qpresent = qpresent[:-1]
-	spresent = spresent[:-1]
-	qaligned = qaligned[:-1]
-	saligned = saligned[:-1]
+	seldict['qpresent'] = seldict['qpresent'][:-1]
+	seldict['spresent'] = seldict['spresent'][:-1]
+	seldict['qaligned'] = seldict['qaligned'][:-1]
+	seldict['saligned'] = seldict['saligned'][:-1]
+	return seldict
 
-	return qmasked, smasked, qpresent, spresent, qaligned, saligned
-
-def main(name, infile):
-	with open(infile) as f:
-		found = False
-		for l in f:
-			if l.startswith(name): 
-				found = l.strip()
-		if not found:
-			print('Error: Could not find {} in {}'.format(name, infile))
-			exit(1)
-	name, jstr = found.split('\t')
-	obj = json.loads(jstr)
-
-	prefix = tempfile.mkdtemp()
-
-	query, qchain, qtms, vs, subject, schain, stms = name.split('_')
-
-	f = urllib.urlopen('https://files.rcsb.org/view/{}.pdb'.format(obj['query']))
-	with open('{}/{}_preimage.pdb'.format(prefix, obj['query']), 'w') as g: g.write(f.read())
-	f = urllib.urlopen('https://files.rcsb.org/view/{}.pdb'.format(obj['subject']))
-	with open('{}/{}.pdb'.format(prefix, obj['subject']), 'w') as g: g.write(f.read())
-	p = subprocess.Popen(['superpose', 
-		'{}/{}_preimage.pdb'.format(prefix, obj['query']), 
-		'-s', '{}/{}-{}'.format(qchain, obj['qpresent'][0][0], obj['qpresent'][0][1]), 
-		'{}/{}.pdb'.format(prefix, obj['subject']), 
-		'-s', '{}/{}-{}'.format(schain, obj['spresent'][0][0], obj['spresent'][0][1]),
-		'-o', '{}/{}.pdb'.format(prefix, obj['query'])
-	], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	out, err = p.communicate()
-
-	for l in out:
-		if 'r.m.s.d' in l: 
-			rmsd = l.strip().split()[1]
-			if rmsd != obj['rmsd']:
-				print('[WARN]: Rerun RMSD ({}) does not match recorded RMSD ({})')
-			break
-
+def main(name, obj, wd=None):
 	sys.argv = sys.argv[:1]
-
 	pymol.finish_launching()
-	pymol.cmd.load('{}/{}.pdb'.format(prefix, obj['query']))
-	pymol.cmd.load('{}/{}.pdb'.format(prefix, obj['subject']))
 
-	qmasked, smasked, qpresent, spresent, qaligned, saligned = extract_selectors(name, obj)
+	if wd is None:
+		print('Error: No output directory specified')
+		exit(1)
 
-	def spview():
-		pymol.cmd.select('qchain', '{} and c. {}'.format(query, qchain))
-		pymol.cmd.select('schain', '{} and c. {}'.format(subject, schain))
-		pymol.cmd.select('qmasked', qmasked)
-		pymol.cmd.select('smasked', smasked)
-		pymol.cmd.select('qpresent', qpresent)
-		pymol.cmd.select('spresent', spresent)
-		pymol.cmd.select('qaligned', qaligned)
-		pymol.cmd.select('saligned', saligned)
-		pymol.cmd.hide('everything')
-		pymol.cmd.show('cartoon', '({}) or ({})'.format(qmasked, qpresent))
-		pymol.cmd.show('cartoon', '({}) or ({})'.format(smasked, spresent))
-		pymol.cmd.color('smudge', query)
-		pymol.cmd.color('lightteal', subject)
-		pymol.cmd.color('forest', 'qchain')
-		pymol.cmd.color('deepteal', 'schain')
-		pymol.cmd.color('palegreen', 'qpresent')
-		pymol.cmd.color('palecyan', 'spresent')
-		pymol.cmd.color('green', 'qaligned')
-		pymol.cmd.color('cyan', 'saligned')
-		pymol.cmd.center('qpresent or spresent')
+	pymol.cmd.set('fetch_path', wd)
+	pymol.cmd.fetch(obj['query'])
+	pymol.cmd.fetch(obj['subject'])
 
-	pymol.cmd.extend('spview', spview)
-	spview()
+	selectors = extract_selectors(name, obj)
+	pymol.cmd.hide('lines')
+	for k in sorted(selectors):
+		if k != 'matrix':
+			pymol.cmd.select(k, selectors[k])
+			if k.endswith('masked'):
+				pymol.cmd.show('cartoon', k)
+
+	truematrix = []
+	for row in obj['matrix']: truematrix += row
+	truematrix += [0., 0., 0., 1.]
+	pymol.cmd.transform_selection(obj['query'], truematrix)
+	pymol.cmd.rotate('x', 90)
+	pymol.cmd.center('*masked')
+	pymol.cmd.zoom('*masked')
+
+	pymol.cmd.color('palegreen', obj['query'])
+	pymol.cmd.color('palecyan', obj['subject'])
+	pymol.cmd.color('tv_green', 'qaligned')
+	pymol.cmd.color('cyan', 'saligned')
+
 	pymol.cmd.deselect()
-	for fn in os.listdir(prefix):
-		os.remove('{}/{}'.format(prefix, fn))
-	os.rmdir(prefix)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
+	parser.add_argument('infile', nargs='+', help='TSVs to read data from')
 
-	parser.add_argument('infile')
-	parser.add_argument('name')
+	parser.add_argument('name', help='name of alignment to load. Be warned that this selects only the first occurrence in all the files in the order given')
 
 	args = parser.parse_args()
 
-	main(args.name, args.infile)
+	found = False
+	jstr = ''
+	for fn in args.infile:
+		with open(fn) as f:
+			for l in f:
+				if l.startswith(args.name):
+					print(args.name, 'found in', fn)
+					jstr = l[l.find('\t')+1:]
+				if found: break
+		if found: break
+
+	if not jstr: 
+		print('Error: Could not find {} in {}'.format(args.name, args.infile))
+
+	obj = json.loads(jstr)
+	wd = tempfile.mkdtemp()
+	try:
+		main(args.name, obj, wd=wd)
+	finally:
+		shutil.rmtree(wd)
